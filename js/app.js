e1181d157f5b16a46ab94130d75d07a130ef87dd
YairMendoza95 <joseyair.95@gmail.com>
2016-12-29 04:02:58 -0600

    Implementacion de la generacion de la lista de canciones y se agregaron las imagenes correspondientes a las canciones

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-
+	GetSongs();
 });
 var audio;
 var music;
+
+function GetSongs(){
+	$.getJSON("js/app.json", function(mjson){
+		music = mjson;
+		//console.log(music);
+		GenList(music);
+	});
+}
+
+function GenList(music){
+	$.each(music.songs, function(i, song){
+		$('#playlist').append('<li class="list-group-item" id="' + i + '">' + song.nombre + '</li>')
+	});
+
+	$('#playlist li').click(function(){
+		var selectedSong = $(this).attr('id');
+		console.log(selectedSong);
+	});
+}
